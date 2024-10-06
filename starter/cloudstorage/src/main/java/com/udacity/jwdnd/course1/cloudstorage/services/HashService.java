79c2c7f41c84f55e6fa553f84550d45ca7a9aefc
DungNguyen <tiendungnguyen.4476@gmail.com>
2024-10-06 16:46:55 +0700

    setup controller, mapper, model, security, services, and tests

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/HashService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/HashService.java
@@ -11,19 +11,16 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Base64;
 
-@Service
+@Component
 public class HashService {
-
-    public final Logger logger = LoggerFactory.getLogger(HashService.class);
+    private Logger logger = LoggerFactory.getLogger(HashService.class);
 
     public String getHashedValue(String data, String salt) {
         byte[] hashedValue = null;
 
-        int iterCount = 12288;
-        int derivedKeyLength = 256;
-        KeySpec spec = new PBEKeySpec(data.toCharArray(), salt.getBytes(), iterCount, derivedKeyLength * 8);
+        KeySpec spec = new PBEKeySpec(data.toCharArray(), salt.getBytes(), 5000, 128);
         try {
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
             hashedValue = factory.generateSecret(spec).getEncoded();
         } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
             logger.error(e.getMessage());
@@ -31,5 +28,4 @@ public class HashService {
 
         return Base64.getEncoder().encodeToString(hashedValue);
     }
-
 }
